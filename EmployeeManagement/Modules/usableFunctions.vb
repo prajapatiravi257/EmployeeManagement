47c9b473f561cb6c612d08ddf0a43ec548996bc2
prajapatiravi257 <prajapatiravi257@gmail.com>
2017-02-22 00:47:48 -0800

    emptybox() added

--- EmployeeManagement/Modules/usableFunctions.vb
+++ EmployeeManagement/Modules/usableFunctions.vb
@@ -1,4 +1,18 @@
 ﻿Module usableFunctions
 
+    Public Function emptyBoxCheck(ByVal form As Form) As Boolean
+        Dim EmptyTextBoxFound As Boolean = False 'Boolean flag for empty textbox 
+        Dim EmptyTextBoxName As String = ""
+
+        For Each ctl As Control In form.Controls
+
+            If TypeOf ctl Is TextBox And ctl.Text.Length = 0 Then
+                EmptyTextBoxName = ctl.Name
+                EmptyTextBoxFound = True
+                Exit For
+            End If
+        Next
+        Return EmptyTextBoxFound
+    End Function
 
 End Module
