26cdf18cc1eb6ec515994fb81b26d77b5ffe95fd
prajapatiravi257 <prajapatiravi257@gmail.com>
2017-02-21 22:53:29 -0800

    removed unneccsary regex function

--- EmployeeManagement/Modules/validation.vb
+++ EmployeeManagement/Modules/validation.vb
@@ -4,7 +4,7 @@ Imports System.Text.RegularExpressions
 Module validation
 
     Public Const rgx_zipcode As String = "^\d{6}$"
-    Public Const rgx_mob As String = "/^(7|8|9)\d{9}$/"
+    Public Const rgx_mob As String = "^(\+91[\-\s]?)?[0]?(91)?[789]\d{9}$"
     Public Const rgx_str As String = "^[A-Za-z]+$"
     Public Const rgx_digit As String = "[^\d]"
 
