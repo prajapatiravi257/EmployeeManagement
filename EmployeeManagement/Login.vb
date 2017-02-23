b1d7047b33729a1be061defdf67a8a83a89754bd
prajapatiravi257 <prajapatiravi257@gmail.com>
2017-02-23 02:50:44 -0800

    minor fixes

--- EmployeeManagement/Login.vb
+++ EmployeeManagement/Login.vb
@@ -1,6 +1,4 @@
-﻿Imports System.Text.RegularExpressions
-
-Public Class Login
+﻿Public Class Login
 
     ' TODO: Insert code to perform custom authentication using the provided username and password 
     ' (See http://go.microsoft.com/fwlink/?LinkId=35339).  
@@ -13,35 +11,13 @@ Public Class Login
     Private Sub OK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK.Click
         userLogin(UsernameTextBox, PasswordTextBox)
         If loginSuccess = True Then
-            Me.Close()
+            Close()
         End If
     End Sub
 
     Private Sub Cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel.Click
-        Me.Close()
+        Close()
     End Sub
 
-    Private Sub LoginForm1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-    End Sub
-
-    Dim errorProvider1 As ErrorProvider = New ErrorProvider()
-
-
-    Private Sub PasswordTextBox_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles PasswordTextBox.Validating
-        If (String.IsNullOrEmpty(PasswordTextBox.Text)) Then
-
-            errorProvider1.SetError(PasswordTextBox, "Password required!")
-
-        ElseIf (Not Regex.IsMatch(PasswordTextBox.Text, "[A-Za-z][A-Za-z0-9]{2,7}")) Then
-
-            errorProvider1.SetError(PasswordTextBox, "Password invalid!")
-
-        Else
-
-            errorProvider1.SetError(PasswordTextBox, Nothing)
-
-        End If
-
-    End Sub
 End Class
