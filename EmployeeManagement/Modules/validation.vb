4e966d751577b6d0fdd3eff80c911596f79c712b
prajapatiravi257 <prajapatiravi257@gmail.com>
2017-02-23 02:51:13 -0800

    improved

--- EmployeeManagement/Modules/validation.vb
+++ EmployeeManagement/Modules/validation.vb
@@ -9,31 +9,6 @@ Module validation
     Public Const rgx_digit As String = "[^\d]"
 
 
-
-    Public Function validationcheck() As Boolean
-        Dim addpersondialog As New dialog_add_person()
-        Dim ErrorProvider As ErrorProvider = New ErrorProvider
-
-
-        Dim isValid As Boolean = False
-        For Each c As Control In addpersondialog.gb_basic_info.Controls
-            If TypeOf c Is TextBox Then
-                If (CType(c, TextBox).Text.Trim() = String.Empty) Then
-                    ErrorProvider.SetError(c, "Required field.")
-                    isValid = False
-                    Exit For
-                Else 'If (CType(c, TextBox).Text.Trim()  String.Empty) Then
-                    ErrorProvider.SetError(c, "")
-                    isValid = True
-                    Continue For
-                End If
-            End If
-        Next
-        Return isValid
-
-    End Function
-
-
     Public Function ValidateEmail(ByVal strCheck As String) As Boolean
         Try
             Dim vEmailAddress As New MailAddress(strCheck)
